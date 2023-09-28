dd6658691ca06e8a50372409391c7b6885b90502
Neil Anderson <njp.anderson@icloud.com>
2023-09-28 20:52:47 +0100

    Enforce link and title

--- src/contracts/ElementCollector.ts
+++ src/contracts/ElementCollector.ts
@@ -1,5 +1,6 @@
+import CollectedElementData from "./CollectedElementData";
 import Collector from "@lib/Collector";
 
 export default interface ElementCollector {
-	(element: Element, collector: Collector): object,
+	(element: Element, collector: Collector): CollectedElementData,
 }
